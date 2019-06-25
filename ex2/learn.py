Fix scaling bug

ex2/learn.py
@@ -50,7 +50,9 @@ np.random.shuffle(data)
 
 x, y = data[:,:-1].astype(np.float32), data[:,-1:].astype(np.uint8)
 means = np.mean(x, axis=0)
+assert means.shape[0] == x.shape[1]
 stds = np.std(x, axis=0)
+assert stds.shape[0] == x.shape[1]
 x = (x-means)/stds
 
 class OurDataset(Dataset):
@@ -166,7 +168,7 @@ def pdp():
 	all_labels = []
 	net.eval()
 
-	pdp_module.pdp(data[:,:-1], lambda x: torch.sigmoid(net(torch.FloatTensor(x).to(device))).detach().unsqueeze(1).cpu().numpy(), features, means=means, stds=stds, resolution=1000, n_data=1000)
+	pdp_module.pdp(x, lambda x: torch.sigmoid(net(torch.FloatTensor(x).to(device))).detach().unsqueeze(1).cpu().numpy(), features, means=means, stds=stds, resolution=1000, n_data=1000)
 
 if __name__=="__main__":
 	cuda_available = torch.cuda.is_available()

ex2/pdp.py
@@ -19,16 +19,18 @@ def pdp(data, eval_function, features, means, stds, resolution=100, n_data=100):
 	downsampled_data = data[index,:]
 
 	pdps = np.zeros((data.shape[1], resolution))
+	print(list(zip(features, list(means))))
 
 	for i, feature in enumerate(features):
-		print ('Processing feature %d: %s' % (i, feature))
 		minimum, maximum = data[:,i].min(), data[:,i].max()
+		minimum_rescaled, maximum_rescaled = minimum*stds[i]+means[i], maximum*stds[i]+means[i]
+		print ('Processing feature %d: %s. Min: %.3f, Max: %.3f' % (i, feature, minimum_rescaled, maximum_rescaled))
 		for j_index, j in enumerate(np.linspace(minimum, maximum, num=resolution)):
 			dd_cpy = downsampled_data.copy()
-			dd_cpy[:,i] = j/resolution
+			dd_cpy[:,i] = j
 			pdps[i,j_index] = np.mean(eval_function(dd_cpy)[:,0])
 
-		rescaled = np.linspace(minimum*stds[i]+means[i], maximum*stds[i]+means[i], num=resolution)
+		rescaled = np.linspace(minimum_rescaled, maximum_rescaled, num=resolution)
 		plt.plot(rescaled, pdps[i,:])
 		plt.xlabel('Normalized feature')
 		plt.ylabel('Mean probability')