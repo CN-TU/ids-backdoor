078a0ea534e6a265c3b523327465e4855683b798
Maximilian Bachl <maximilian.bachl@nt.tuwien.ac.at>
2019-07-05 20:04:36 +0200

    Finish train and test

--- ex3/learn.py
+++ ex3/learn.py
@@ -5,6 +5,7 @@ import os
 import pickle
 import math
 import argparse
+import random
 
 import numpy as np
 import torch
@@ -56,24 +57,15 @@ class OurLSTMModule(nn.Module):
 
 	# batch has seq * batch * input_dim
 
-	def init_hidden(self, probability=0.0):
+	def init_hidden(self, batch_size, probability=0.0):
 
-		if self.hidden is not None:
-			probs_tensor = torch.zeros(self.n_layers, self.batch_size, self.hidden_size)
-			probs_tensor.fill_(probability)
-			drawn1 = torch.bernoulli(probs_tensor)
-			drawn2 = torch.bernoulli(probs_tensor)
-
-			self.hidden = ((drawn1.to(device)*self.hidden[0]).detach().to(device),
-			(drawn2.to(device)*self.hidden[1]).detach().to(device))
-
-		else:
-			self.hidden = (torch.zeros(self.n_layers, self.batch_size, self.hidden_size).to(self.device),
-			torch.zeros(self.n_layers, self.batch_size, self.hidden_size).to(self.device))
+		self.hidden = (torch.zeros(self.n_layers, batch_size, self.hidden_size).to(self.device),
+		torch.zeros(self.n_layers, self.batch_size, self.hidden_size).to(self.device))
 
 	def forward(self, batch):
 
 		# preprocessed_batch = self.i2h(batch.view(-1,batch.shape[-1])).view(batch.shape[0], batch.shape[1], self.hidden_size)
+		# print("batch", batch)
 		lstm_out, self.hidden = self.lstm(batch, self.hidden)
 		lstm_out, seq_lens = torch.nn.utils.rnn.pad_packed_sequence(lstm_out)
 		# output = self.h2o(lstm_out.view(-1, self.hidden_size)).view(*lstm_out.shape[:2], self.num_outputs)
@@ -87,43 +79,18 @@ def get_one_hot_vector(class_indices, num_classes, batch_size):
 	return y_onehot.scatter_(1, class_indices.unsqueeze(1), 1)
 
 def custom_collate(seqs):
-	# get the length of each seq in your batch
+	# print("seqs", seqs)
+	# quit()
+	# seqs = [(item[0][:opt.maxLength,:], item[1][:opt.maxLength,:]) for item in seqs]
 	seqs, labels = zip(*seqs)
 	assert len(seqs) == len(labels)
-	# print("seqs", seqs)
-	seq_lengths = torch.LongTensor([len(seq) for seq in seqs])
+	seq_lengths = torch.LongTensor([len(seq) for seq in seqs]).to(device)
 
-	# TODO: Use built-in padding function?
-	# dump padding everywhere, and place seqs on the left.
-	# NOTE: you only need a tensor as big as your longest sequence
-	# seq_tensor = torch.zeros((len(seqs), seq_lengths.max(), seqs[0].shape[-1]))
-	# for idx, (seq, seqlen) in enumerate(zip(seqs, seq_lengths)):
-	# 	# print("seq", seq.shape, "seqlen", seqlen)
-	# 	assert seqlen != 0, "seqlen: {}".format(seqlen)
-	# 	seq_tensor[idx, :seqlen, :] = torch.FloatTensor(seq).squeeze()
+	# print("seq_lengths", seq_lengths)
 
-	# labels_tensor = torch.zeros((len(labels), seq_lengths.max(), labels[0].shape[-1]))
-	# for idx, (label, seqlen) in enumerate(zip(labels, seq_lengths)):
-	# 	# print("seq", seq.shape, "seqlen", seqlen)
-	# 	assert seqlen != 0, "seqlen: {}".format(seqlen)
-	# 	labels_tensor[idx, :seqlen, :] = torch.FloatTensor(label)
+	seq_tensor = torch.nn.utils.rnn.pad_sequence(seqs).to(device)
+	labels_tensor = torch.nn.utils.rnn.pad_sequence(labels).to(device)
 
-	seq_tensor = torch.nn.utils.rnn.pad_sequence(seqs)
-	labels_tensor = torch.nn.utils.rnn.pad_sequence(labels)
-
-	# # SORT YOUR TENSORS BY LENGTH!
-	# seq_lengths, perm_idx = seq_lengths.sort(0, descending=True)
-	# seq_tensor = seq_tensor[perm_idx]
-	# labels_tensor = labels_tensor[perm_idx]
-
-	# utils.rnn lets you give (B,L,D) tensors where B is the batch size, L is the maxlength, if you use batch_first=True
-	# Otherwise, give (L,B,D) tensors
-	# seq_tensor = seq_tensor.transpose(0,1) # (B,L,D) -> (L,B,D)
-	# labels_tensor = labels_tensor.transpose(0,1) # (B,L,D) -> (L,B,D)
-
-	# pack them up nicely
-	# packed_input = pack_padded_sequence(seq_tensor, seq_lengths.cpu().numpy())
-	# print("seq_tensor.shape", seq_tensor.shape, "seq_lengths.shape", seq_lengths.shape)
 	packed_input = torch.nn.utils.rnn.pack_padded_sequence(seq_tensor, seq_lengths, enforce_sorted=False)
 	packed_labels = torch.nn.utils.rnn.pack_padded_sequence(labels_tensor, seq_lengths, enforce_sorted=False)
 	return packed_input, packed_labels
@@ -139,7 +106,6 @@ def train():
 	train_loader = torch.utils.data.DataLoader(train_data, batch_size=opt.batchSize, shuffle=True, collate_fn=custom_collate)
 
 	optimizer = optim.SGD(lstm_module.parameters(), lr=opt.lr)
-	# criterion = nn.CrossEntropyLoss(reduction="mean")
 	criterion = nn.BCEWithLogitsLoss(reduction="mean")
 
 	writer = SummaryWriter()
@@ -147,46 +113,50 @@ def train():
 	samples = 0
 	for i in range(1, sys.maxsize):
 		for input_data, labels in train_loader:
-			print("iterating")
-			samples += len(input_data)
+			# print("iterating")
+			# samples += len(input_data)
 			optimizer.zero_grad()
-			lstm_module.init_hidden()
+			batch_size = input_data.sorted_indices.shape[0]
+			assert batch_size <= opt.batchSize, "batch_size: {}, opt.batchSize: {}".format(batch_size, opt.batchSize)
+			lstm_module.init_hidden(batch_size)
 
 			# actual_input = torch.FloatTensor(input_tensor[:,:,:-1]).to(device)
 
+			# print("input_data.data.shape", input_data.data.shape)
 			output, seq_lens = lstm_module(input_data)
+
+			# torch.set_printoptions(profile="full")
+			# print("output", output.detach().squeeze().transpose(1,0))
+
+			samples += output.shape[1]
 
 			index_tensor = torch.arange(0, output.shape[0], dtype=torch.int64).unsqueeze(1).unsqueeze(2).repeat(1, output.shape[1], output.shape[2])
 
-			selection_tensor = seq_lens.unsqueeze(0).unsqueeze(2).repeat(index_tensor.shape[0], 1, index_tensor.shape[2])
+			selection_tensor = seq_lens.unsqueeze(0).unsqueeze(2).repeat(index_tensor.shape[0], 1, index_tensor.shape[2])-1
 
 			# print("index_tensor.shape", index_tensor.shape, "selection_tensor.shape", selection_tensor.shape)
-			mask = (index_tensor < selection_tensor).byte()
-
-			torch.set_printoptions(profile="full")
+			mask = (index_tensor <= selection_tensor).byte().to(device)
+			mask_exact = (index_tensor == selection_tensor).byte().to(device)
+			# torch.set_printoptions(profile="full")
 			# print("mask", mask.squeeze())
-
-			mask_exact = (index_tensor == selection_tensor).byte()
-
-			# labels = torch.LongTensor(input_tensor[:,:,-1])
-			# loss = criterion(output.view(-1, output.shape[-1]), labels.view(-1))
-			# print("labels.shape", labels.shape, "actual_input.shape", actual_input.shape)
-
 			labels, _ = torch.nn.utils.rnn.pad_packed_sequence(labels)
 
-			# print("output.shape", output.shape, "labels.shape", labels.shape, "mask.shape", mask.shape)
-			loss = criterion(output[mask].view(-1), labels[mask].view(-1))#.view(labels.shape[0], labels.shape[1], lstm_module.num_outputs).to(device))
+			loss = criterion(output[mask].view(-1), labels[mask].view(-1))
 			labels = labels.to(device)
 			loss.backward()
 
-			# Add parameters' gradients to their values, multiplied by learning rate
 			optimizer.step()
 
-			writer.add_scalar("loss", loss.item(), i*opt.batchSize)
-			accuracy = torch.mean((torch.round(output.detach()[mask]) == labels[mask]).float())
-			writer.add_scalar("accuracy", accuracy, i*opt.batchSize)
-			end_accuracy = torch.mean((torch.round(output.detach()[mask_exact]) == labels[mask_exact]).float())
-			writer.add_scalar("end_accuracy", end_accuracy, i*opt.batchSize)
+			# print("masked_output_shape", torch.round(output.detach()[mask]).shape, "masked_labels_shape", labels[mask].shape)
+			# print("exact_masked_output_shape", torch.round(output.detach()[mask_exact]).shape, "exact_masked_labels_shape", labels[mask_exact].shape)
+			# print("output.shape", output.shape, "labels.shape", labels.shape)
+			assert output.shape == labels.shape
+			writer.add_scalar("loss", loss.item(), samples)
+			sigmoided_output = torch.sigmoid(output.detach())
+			accuracy = torch.mean((torch.round(sigmoided_output[mask]) == labels[mask]).float())
+			writer.add_scalar("accuracy", accuracy, samples)
+			end_accuracy = torch.mean((torch.round(sigmoided_output[mask_exact]) == labels[mask_exact]).float())
+			writer.add_scalar("end_accuracy", end_accuracy, samples)
 
 			# confidence_for_correct_one = torch.mean(torch.gather(torch.sigmoid(output.detach()[mask]), 2, labels[mask]))
 			# writer.add_scalar("confidence", confidence_for_correct_one, i*opt.batchSize)
@@ -210,9 +180,57 @@ def train():
 			# 	sys.stdout.flush()
 			# 	sys.stderr.flush()
 
-			if i % 100 == 0:
-				torch.save(lstm_module.state_dict(), '%s/lstm_module_%d.pth' % (writer.log_dir, i*opt.batchSize))
+		# Save after every epoch
+		if i % 1 == 0:
+			torch.save(lstm_module.state_dict(), '%s/lstm_module_%d.pth' % (writer.log_dir, i))
 
+def test():
+
+	n_fold = opt.nFold
+	fold = opt.fold
+	lstm_module.eval()
+
+	_, test_indices = get_nth_split(dataset, n_fold, fold)
+	test_data = torch.utils.data.Subset(dataset, test_indices)
+	test_loader = torch.utils.data.DataLoader(test_data, batch_size=opt.batchSize, shuffle=False, collate_fn=custom_collate)
+
+	all_accuracies = []
+	all_end_accuracies = []
+	samples = 0
+	for index, (input_data, labels) in enumerate(test_loader):
+
+		batch_size = input_data.sorted_indices.shape[0]
+		assert batch_size <= opt.batchSize, "batch_size: {}, opt.batchSize: {}".format(batch_size, opt.batchSize)
+		lstm_module.init_hidden(batch_size)
+
+		output, seq_lens = lstm_module(input_data)
+
+		samples += output.shape[1]
+
+		index_tensor = torch.arange(0, output.shape[0], dtype=torch.int64).unsqueeze(1).unsqueeze(2).repeat(1, output.shape[1], output.shape[2])
+
+		selection_tensor = seq_lens.unsqueeze(0).unsqueeze(2).repeat(index_tensor.shape[0], 1, index_tensor.shape[2])-1
+
+		mask = (index_tensor <= selection_tensor).byte().to(device)
+		mask_exact = (index_tensor == selection_tensor).byte().to(device)
+
+		labels, _ = torch.nn.utils.rnn.pad_packed_sequence(labels)
+
+		labels = labels.to(device)
+
+		assert output.shape == labels.shape
+
+		sigmoided_output = torch.sigmoid(output.detach())
+		accuracy_items = torch.round(sigmoided_output[mask]) == labels[mask]
+		accuracy = torch.mean(accuracy_items.float())
+		end_accuracy_items = torch.round(sigmoided_output[mask_exact]) == labels[mask_exact]
+		end_accuracy = torch.mean(end_accuracy_items.float())
+
+		all_accuracies.append(accuracy_items.cpu().numpy())
+		all_end_accuracies.append(end_accuracy_items.cpu().numpy())
+
+	print("per-packed accuracy", np.mean(np.concatenate(all_accuracies)))
+	print("per-flow end-accuracy", np.mean(np.concatenate(all_end_accuracies)))
 
 if __name__=="__main__":
 	parser = argparse.ArgumentParser()
@@ -223,26 +241,35 @@ if __name__=="__main__":
 	parser.add_argument('--batchSize', type=int, default=128, help='input batch size')
 	parser.add_argument('--net', default='', help="path to net (to continue training)")
 	parser.add_argument('--function', default='train', help='the function that is going to be called')
-	parser.add_argument('--manualSeed', type=int, help='manual seed')
+	parser.add_argument('--manualSeed', default=0, type=int, help='manual seed')
+	parser.add_argument('--maxLength', type=int, default=1000, help='max length')
+	parser.add_argument('--maxSize', type=int, default=sys.maxsize, help='limit of samples to consider')
 	parser.add_argument('--lr', type=float, default=10**(-2), help='learning rate')
 
 	opt = parser.parse_args()
+	print(opt)
+	SEED = opt.manualSeed
+	random.seed(SEED)
+	np.random.seed(SEED)
+	torch.manual_seed(SEED)
 
 	with open (opt.dataroot, "rb") as f:
 		all_data = pickle.load(f)
 
+	all_data = [item[:opt.maxLength,:] for item in all_data]
+	random.shuffle(all_data)
+	all_data = all_data[:opt.maxSize]
+	# print("lens", [len(item) for item in all_data])
 	x = [item[:, :-2] for item in all_data]
 	y = [item[:, -1:] for item in all_data]
 
 	if opt.normalizationData == "":
 		file_name = opt.dataroot[:-7]+"_normalization_data.pickle"
 		catted_x = np.concatenate(x, axis=0)
-		# print("catted_x.shape", catted_x.shape)
 		means = np.mean(catted_x, axis=0)
 		stds = np.std(catted_x, axis=0)
 		stds[stds==0.0] = 1.0
-		# np.set_printoptions(suppress=True)
-		# stds[np.isclose(stds, 0)] = 1.0
+
 		with open(file_name, "wb") as f:
 			f.write(pickle.dumps((means, stds)))
 	else:
