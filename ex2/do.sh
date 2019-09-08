da63bc4d13ae62e40d86318b45028c7171e86599
Maximilian Bachl <maximilian.bachl@nt.tuwien.ac.at>
2019-09-08 23:27:34 +0200

    Increase granularity in do.sh

--- ex2/do.sh
+++ ex2/do.sh
@@ -18,7 +18,7 @@ done
 wait
 
  # PRUNING
-valSizes='0.01 0.02 0.05 0.1 0.2 0.5 1'
+valSizes='0.01 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1'
 for ds in 15 17; do
 	for i in 0 1 2; do
 		for valSize in $valSizes; do
