464e46d62a076698f10519d99f6972ba90aeda77
Alexander Hartl <alexander.hartl@tuwien.ac.at>
2019-09-09 19:29:34 +0200

    Added plotting scripts

--- ex2/plot_finetuning.py
+++ ex2/plot_finetuning.py
@@ -7,18 +7,23 @@ import numpy as np
 import pandas as pd
 
 
-prefix = sys.argv[1]
+plt.rcParams["font.family"] = "serif"
 
-data = [ pd.read_csv('%s_%d_3/finetuning.csv' % (prefix, i)) for i in range(3) ]
+data = [ pd.read_csv(filename) for filename in sys.argv[2:] ]
 columns = data[0].columns
+columns = {'Youden': "J score", 'Backdoor_acc': 'Backdoor accuracy'}
 length = min([ item.shape[0] for item in data ])
-data = np.stack([ item.loc[:length,columns].values for item in data ]) # order of metrics might deviate in files
+data = np.stack([ item.iloc[:length,:].loc[:,list(columns)].values for item in data ]) # order of metrics might deviate in files
 
 means = np.mean(data, axis=0)
 stds = np.std(data, axis=0)
 
-plt.errorbar(np.arange(length) + 1, means, stds)
+plt.figure(figsize=(5,2))
+for i in range(means.shape[1]):
+	plt.errorbar(np.arange(length)[:,None] + 1, means[:,i], stds[:,i])
 plt.xlabel('Epoch')
 plt.ylabel('Metric')
-plt.legend(columns)
-plt.show()
+plt.legend(columns.values())
+plt.tight_layout()
+plt.savefig(sys.argv[1])
+#plt.show()
