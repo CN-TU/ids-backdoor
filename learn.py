aa14f6ebf7a4da95690350e0157c873c3c79abeb
Maximilian Bachl <maximilian.bachl@nt.tuwien.ac.at>
2020-07-09 12:21:39 +0200

    Fix merge

--- learn.py
+++ learn.py
@@ -329,15 +329,14 @@ def train_eager_stopping_nn():
 			total_loss = None
 			eager_stopping_weight_per_output_per_layer = globals()[opt.eagerStoppingWeightingMethod](len(losses))
 
-			#for loss_index, loss in enumerate(losses):
-			#	loss = eager_stopping_weight_per_output_per_layer[loss_index]*loss
-			#	if total_loss is None:
-			#		total_loss = loss
-			#	else:
-			#		total_loss += loss
-			total_loss = np.sum(np.multiply(losses, eager_stopping_weight_per_output_per_layer))
+			for loss_index, loss in enumerate(losses):
+				loss = eager_stopping_weight_per_output_per_layer[loss_index]*loss
+				if total_loss is None:
+					total_loss = loss
+				else:
+					total_loss += loss
 
-			total_loss.backward() # correct way to do it? back-propagate total from last layer?
+			total_loss.backward()
 			optimizer.step()
 
 			writer.add_scalar("loss", total_loss.item(), samples)
